909a5f00521255e90f8a18c3f4486430cc48c4a4
Antony Durán Hernández <ajdurancr@gmail.com>
2016-04-03 09:53:20 -0600

    se crea método de ejecución de peticiones http genérica

--- dataAccess/connection.js
+++ dataAccess/connection.js
@@ -1,1 +1,30 @@
-//http methods file
+//importa paquete request para hacer las peticiones http
+var request = require("request");
+
+exports.httpRequest = function(config, data, callback){
+    /*
+        config:{
+            uri, //dirección url del método
+            method // GET, POST, PUT, DELETE
+        }
+
+        data // json con la información a enviar
+    */
+
+    request({
+      uri: config.uri,
+      method: config.method,
+      form: { data }
+    }, function(error, res, data) {
+      if(error){
+        callback({
+            success: false,
+            data: null, 
+            message: 400
+        });
+      } else {
+        callback(data);
+      }
+    });
+
+}
