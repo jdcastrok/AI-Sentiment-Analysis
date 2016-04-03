abff7eabf628a8abedf1118a47c9389a8803bab4
David Castro <jdcastrok@gmail.com>
2016-04-03 12:04:28 -0600

    agregadas plantillas de funciones para learning en el dataAccess y el modulo mathService en el businessLogic

---
+++ businessLogic/mathService.js
@@ -0,0 +1,3 @@
+exports.updateModels = function(callback) {
+  callback({'resultCode':200});
+}
---
+++ dataAccess/learningRepository.js
@@ -0,0 +1,15 @@
+exports.getStopWords = function(callback) {
+  callback([{'word':'hehe'},{'word':'my'},{'word':'lol'}]);
+};
+
+exports.createBackup = function(collections,callback) {
+  callback({'resultCode':200});
+};
+
+exports.updateHistoricals = function(data,callback) {
+  callback({'resultCode':200});
+};
+
+exports.restoreBackup = function(collections,callback) {
+  callback({'resultCode':200});
+};
