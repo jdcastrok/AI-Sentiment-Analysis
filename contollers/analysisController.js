d70677082b4f7448cbdde1a8b8c9a5a7b5e7b33e
Antony Durán Hernández <ajdurancr@gmail.com>
2016-04-04 23:34:40 -0600

    declaración de método analyzeText

--- contollers/analysisController.js
+++
@@ -1,7 +0,0 @@
-var analysisService = require('../businessLogic/analysisService.js'); 
-
-exports.analyzeText = function(aRequest, aResponse) {
-  analysisService.analyzeText(aRequest.params, function(data){
-        aResponse.send(data);
-    });
-};
