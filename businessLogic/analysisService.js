4b16114ee85ae509877033deb5a99c50561f3536
Antony Durán Hernández <ajdurancr@gmail.com>
2016-04-04 23:33:52 -0600

    implementación del método getStopWords

--- businessLogic/analysisService.js
+++ businessLogic/analysisService.js
@@ -1,7 +1,12 @@
 var analysisRepository = require('../dataAccess/analysisRepository.js'); 
 
-exports.analyzeText = function(params, callback) {
-  analysisRepository.getOneMethod(params, function(data){
+/*exports.analyzeText = function(params, callback) {
+  analysisRepository.getOneMethod(params, function(data)r
         callback(data);
     });
-};
+};*/
+
+analysisRepository.getStopWords(function (res) {
+  console.log('analysisRepository.getStopWords:');
+  console.log(res);
+});
