66d9a97d9b74f59438d79a592a2a04e8ffd32c80
maovares <mrodriguezovares@gmail.com>
2016-04-07 07:56:43 -0600

    Inicio para proceso de evaluación (obtener stopwotks, tokenizar, eliminar stopworks y agrupar)

--- businessLogic/analysisService.js
+++ businessLogic/analysisService.js
@@ -1,4 +1,6 @@
-var analysisRepository = require('../dataAccess/analysisRepository.js'); 
+'use strict'
+
+var dataAccess = require('../dataAccess/analysisRepository.js');
 
 /*exports.analyzeText = function(params, callback) {
   analysisRepository.getOneMethod(params, function(data)r
@@ -6,7 +8,36 @@ var analysisRepository = require('../dataAccess/analysisRepository.js');
     });
 };*/
 
+/*
 analysisRepository.getStopWords(function (res) {
   console.log('analysisRepository.getStopWords:');
   console.log(res);
-});
+}); */
+
+
+exports.analyzeText = function(request, callback){
+  var textsArray = request.texts;
+  dataAccess.getStopWords(function (response) {
+    if (response.success) {
+      var sentimentWords = {'words' : []};
+        //transformo cada texto para obtener sus palabras, agruparlas por apariciones
+        for (var i = 0; i < textsArray.length; i++) {
+          var wordOcurrences = utilitiesService.groupByOcurrences(utilitiesService.removeStopWords(utilitiesService.tokenize(textsArray[i].text),response.data));
+          sentimentWords.words = utilitiesService.groupBySentiment(sentimentWords.words, wordOcurrences);
+        }
+
+        dataAccess.getModels(function(response){
+          if(response.success){
+
+          }else {
+            //ERROR obteniendo modelos
+          }
+        });
+
+
+    }else {
+      //ERROR obteniendo stopWords
+    }
+  });
+
+}
