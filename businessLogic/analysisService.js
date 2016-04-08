dcd691b16dc6ffe324dddbe9543db5d443029a77
maovares <mrodriguezovares@gmail.com>
2016-04-07 18:40:00 -0600

    Añadida secuencia para incio de analisis

--- businessLogic/analysisService.js
+++ businessLogic/analysisService.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var dataAccess = require('../dataAccess/analysisRepository.js');
+var utilitiesService = require('./utilitiesService.js');
 
 /*exports.analyzeText = function(params, callback) {
   analysisRepository.getOneMethod(params, function(data)r
@@ -28,6 +29,11 @@ exports.analyzeText = function(request, callback){
 
         dataAccess.getModels(function(response){
           if(response.success){
+            //model = response.models = {'neg' : [], 'pos' : []}
+            var expectedPos = utilitiesService.wordsIntersect = function(sentimentWords.words, response.models.pos);
+            var expectedNeg = utilitiesService.wordsIntersect = function(sentimentWords.words, response.models.neg);
+
+            
 
           }else {
             //ERROR obteniendo modelos
@@ -36,8 +42,23 @@ exports.analyzeText = function(request, callback){
 
 
     }else {
-      //ERROR obteniendo stopWords
+      console.log("ERROR");
     }
   });
-
 }
+
+/*
+var test = function(request){
+  var response = {data:[{word:'nh'}]};
+  var textsArray = request.texts;
+  var sentimentWords = {'words' : []};
+
+    //transformo cada texto para obtener sus palabras, agruparlas por apariciones
+    for (var i = 0; i < textsArray.length; i++) {
+      var wordOcurrences = utilitiesService.groupByOcurrences(utilitiesService.removeStopWords(utilitiesService.tokenize(textsArray[i].text),response.data));
+      sentimentWords.words = utilitiesService.groupBySentiment(sentimentWords.words, wordOcurrences);
+    }
+    console.log(sentimentWords);
+}
+test({texts:[{text:"ij nh bg vf hhb"},{text:"ksd7 hds6 sdhc ystd6 jha"},{text:"mo ko ih y g"}]});
+*/
