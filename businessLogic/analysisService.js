6d23fe6dcf5a0c1be46506a90004d143eae09cb3
Antony Durán Hernández <ajdurancr@gmail.com>
2016-04-12 00:50:35 -0600

    ejecuciones de ejemplo de los métodos del repositorio 'sentimentRepository'

--- businessLogic/analysisService.js
+++ businessLogic/analysisService.js
@@ -1,12 +1,50 @@
-var analysisRepository = require('../dataAccess/analysisRepository.js'); 
+var sentimentRepository = require('../dataAccess/sentimentRepository.js'); 
 
 /*exports.analyzeText = function(params, callback) {
-  analysisRepository.getOneMethod(params, function(data)r
+  sentimentRepository.getOneMethod(params, function(data)r
         callback(data);
     });
 };*/
 
-analysisRepository.getStopWords(function (res) {
-  console.log('analysisRepository.getStopWords:');
+/*
+//ejecución de ejemplo
+sentimentRepository.getStopWords(function (res) {
+  console.log('sentimentRepository.getStopWords:');
   console.log(res);
 });
+
+//ejecución de ejemplo
+sentimentRepository.updateKnowledgeDB(
+    'Model',
+    {
+      "pos": [{"word": "casa", "ocurrences": 243},{"word": "verdad1", "ocurrences": 4}],
+      "neg": [{"word": "inundación", "ocurrences": 34}, {"word": "problema8", "ocurrences": 1534}]
+    }, 
+    function (res) {
+      console.log('sentimentRepository.updateKnowledgeDB: Model');
+      console.log(res);
+    });
+
+//ejecución de ejemplo
+sentimentRepository.updateKnowledgeDB(
+    'Historical',
+    {
+      "pos": [{"word": "casa", "ocurrences": 243},{"word": "verdad1", "ocurrences": 4}],
+      "neg": [{"word": "inundación", "ocurrences": 34}, {"word": "problema8", "ocurrences": 1534}]
+    }, 
+    function (res) {
+      console.log('sentimentRepository.updateKnowledgeDB: Historical');
+      console.log(res);
+     
+    });
+
+sentimentRepository.getKnowledgeDB('Model', function (res) {
+  console.log('sentimentRepository.getKnowledgeDB: Model');
+  console.log(res);
+});
+
+ sentimentRepository.getKnowledgeDB('Historical', function (res) {
+  console.log('sentimentRepository.getKnowledgeDB: Historical');
+  console.log(res);
+});
+*/
