bed49b4bab78bbc0bab6c01b035dcaf16ee5e69a
Antony Durán Hernández <ajdurancr@gmail.com>
2016-04-12 00:47:34 -0600

    se agrega método genérico de conexiones http hacia el módulo de acceso a datos

--- dataAccess/connection.js
+++ dataAccess/connection.js
@@ -1,31 +1,31 @@
-//importa paquete request para hacer las peticiones http
-var request = require("request");
+//importa paquete needle para hacer las peticiones http
+var needle = require('needle');
 
-exports.httpRequest = function(config, data, callback){
-    /*
-        config:{
-            uri, //dirección url del método
-            method // GET, POST, PUT, DELETE
+/*     
+Entrada:
+        config{
+            uri, // ejemplo: 'http://localhost:9000/sentimentAnalysis/v1/updateCollection/'
+            method // nombre del método: GET, POST, PUT, DELETE
+        }  
+        body  //puede ser null si se usa GET, en caso contrario todo el contenido del body se tiene que convertir a string con el método JSON.stringify y cada key tiene que ir definida con comillas dobles ("), por ejemplo
+            {
+                "key1": JSON.stringify(['stringValue', 243, true]), 
+                "key2": JSON.stringify([{"word": "inundación", "ocurrences": 34}])
+            }, 
+            callback // function(res){...} -> recibe la respuesta (res) para ser administrada externamente
+*/
+exports.httpRequest = function(config, body, callback){
+    needle.request(config.method, config.uri, body, function (err, res) {
+        if (err) {
+            callback({
+                success: false,
+                data: null,
+                message: 400
+            })
+        } else {
+            callback(res.body)
         }
-
-        data // json con la información a enviar
-    */
-    console.log('httpRequest: data');
-    console.log(data);
-    request({
-      uri: config.uri,
-      method: config.method,
-      form: data
-    }, function(error, res, data) {
-      if(error){
-        callback({
-            success: false,
-            data: null, 
-            message: 400
-        });
-      } else {
-        callback(data);
-      }
     });
+};
 
-}
+
