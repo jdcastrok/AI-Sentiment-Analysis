b60b53e6964667badceb4ec324daa66280a58a5f
David <jdcastrok@gmail.com>
2016-04-17 03:34:18 -0600

    cambio de nombre de variables, ocur a occur

--- businessLogic/utilitiesService.js
+++ businessLogic/utilitiesService.js
@@ -32,7 +32,7 @@ exports.removeStopWords = function(words, stopWords) {
 //recibe una palabra y determina si es un StopWord
 function isStopWord(word, stopWords) {
   for (var i = 0; i < stopWords.length; i++) {
-    if (word == stopWords[i].word) {
+    if (word == stopWords[i].word.toLowerCase()) {
       return true;
     }
   }
@@ -40,7 +40,7 @@ function isStopWord(word, stopWords) {
 }
 
 //agrupa una lista de palabras en base a su apariciones
-exports.groupByOcurrences = function(words) {
+exports.groupByOccurrences = function(words) {
   var groupedWords = [];
 
   for (var i = 0; i < words.length; i++) {
@@ -48,30 +48,31 @@ exports.groupByOcurrences = function(words) {
     var j = 0;
     for (j; j < groupedWords.length; j++) {
       if (groupedWords[j].word == word) {
-        groupedWords[j].ocur += 1;
+        groupedWords[j].occur += 1;
         break;
       }
     }
     if (j == groupedWords.length) {
-      groupedWords[j] = {"word" : word, "ocur" : 1};
+      groupedWords[j] = {"word" : word, "occur" : 1};
     }
   }
 
   return groupedWords;
 }
 
+//agrega las palabras de wordsToAdd a currentWords, si ya existen, suma al numero de occurrences
 exports.groupBySentiment = function(currentWords, wordsToAdd) {
   for (var i = 0; i < wordsToAdd.length; i++) {
     var word = wordsToAdd[i];
     var j = 0;
     for (j; j < currentWords.length; j++) {
       if (currentWords[j].word == word.word) {
-        currentWords[j].ocur += word.ocur;
+        currentWords[j].occur += word.occur;
         break;
       }
     }
     if (j == currentWords.length) {
-      currentWords[j] = {"word" : word.word, "ocur" : word.ocur};
+      currentWords[j] = {"word" : word.word, "occur" : word.occur};
     }
   }
 
