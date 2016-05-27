dddc26a1c3ec2adafc555c5d01dc93fb002b17f5
maovares <mrodriguezovares@gmail.com>
2016-05-26 22:05:52 -0600

    Añadidos logs para nPerToTake

--- businessLogic/learningService.js
+++ businessLogic/learningService.js
@@ -23,10 +23,10 @@ var genModel = require('./genModelLearningService.js');
 //        }
 //      ] | null
 //  }
-exports.learn = function (request, callback) {
+exports.learn = function (request, nPer,nPerToTake, callback) {
   console.log("1.Inside -> Learn");
   if (request.type == "auto") {
-    autonomousLearn(function (response) {
+    autonomousLearn(nPer,nPerToTake,function (response) {
       if (response.success) {
         //se realizó exitosamente el aprendizaje
       } else {
@@ -36,17 +36,35 @@ exports.learn = function (request, callback) {
     });
   } else if (request.type == "controlled") {
     console.log("2.Call -> Controlled");
-    controlledLearn(request.data, function (response) {
+    controlledLearn(request.data, nPer,nPerToTake, function (response) {
       if (response.success) {
         //exito
-        ///LOGS
+        //////////////////////////////////////////////////////////LOGS
         var today = new Date();
         today = today.getTime();
 
         response.type = request.type;
         response.time = today;
-        /////
-        callback(response);
+
+
+        ////LOGS
+        console.log("23.Call logs");
+        ////
+        updateLogs(response, response.type,function(res){
+          console.log("24.Inside logs");
+          if(res.success){
+            console.log("25.1 logs");
+            callback(res);
+          }else {
+            console.log("25.2 logs");
+            res.data = "Logs db error!";
+            res.message = 1044;
+            callback(res);
+          }
+        });
+
+        ///////////////////////////////////////////////////////////////
+        //callback(response);
       } else {
         //fracaso
         callback(response);
@@ -63,7 +81,7 @@ exports.learn = function (request, callback) {
 //clasifies its words into the respective sentiment
 //when all words are grouped by sentiment
 //call the next common steps and waits for the response
-function autonomousLearn(callback) {
+function autonomousLearn(nPer,nPerToTake,callback) {
   //debería bloquearse la BD
 
   dataAccess.getLearningQueue (function (response) {
@@ -84,7 +102,7 @@ function autonomousLearn(callback) {
       }
 
       //calls the next steps (common to both types of learning)
-      commonLearning(sentimentWords, function (response) {
+      commonLearning(sentimentWords, nPer,nPerToTake, function (response) {
         if (response.success) {
           //todo bien
           //vacío la colección learningQueue
@@ -117,7 +135,7 @@ function autonomousLearn(callback) {
 //poco representaivas (stopwords), se cuenta el número de apariciones para cada palabra distinta
 //finalmente se agrupan las palabras de todos los textos por sentiemiento,
 //según lo indique la llave "sent"
-function controlledLearn(request, callback) {
+function controlledLearn(request, nPer, nPerToTake, callback) {
   console.log("3.Inside Controlled Learning");
   var textsArray = request.data;
   //debería hacerse un lock a la BDtrue
@@ -138,7 +156,7 @@ function controlledLearn(request, callback) {
       var sentimentWords = getWordsPerSentiment(stopWords, request);
       //calls the next steps (common to both types of learning)
       console.log("6.Call -> commonLearning");
-      commonLearning(sentimentWords, function (response) {
+      commonLearning(sentimentWords, nPer,nPerToTake, function (response) {
         if (response.success) {
           //todo bien
           response.stopWords = stopWords;
@@ -160,7 +178,7 @@ function controlledLearn(request, callback) {
 //generates both model collections
 //updates all collections on DB
 //asserts if everything went OK and unlocks the DB
-function commonLearning(newWords, callback) {
+function commonLearning(newWords,nPer,nPerToTake, callback) {
 
   ////// LOGS
   console.log("7.Inside -> commonLearning");
@@ -178,7 +196,7 @@ function commonLearning(newWords, callback) {
       var historicals = updateHistoricals(response.data, newWords);
       //se generan las colecciones modelo
       console.log("13.Call -> generateModel");
-      genModel.generateModel(historicals, function (response) {
+      genModel.generateModel(historicals, nPer,nPerToTake,  function (response) {
         console.log("15.Response -> generateModel");
         if (response.success) {
           //las colecciones modelo se generaron correctamente
@@ -275,18 +293,28 @@ function updateDB(historicals, models, callback) {
 
         ////// LOGS
         console.log("21.1.Response -> updateKnowledgeDB MODEL");
-        console.log(response);
+        //console.log(response);
         response.models = models;
         response.historicals = historicals;
-        //////
 
         //ambas se insertaron exitosamente
         callback(response);
       });
     } else {
       console.log("21.2.Response -> updateKnowledgeDB MODEL");
-      console.log(response);
+      //console.log(response);
       callback(response);
     }
+  });
+}
+
+
+function updateLogs(data, typeLogs, callback){
+  dataAccess.updateLogs(data, typeLogs,function(response){
+    if(response.success){
+			callback({"success": true,"data": null,"message": 200});
+		}else {
+			callback({"success": false,"data": null,"message": 400});
+		}
   });
 }
